e93b7d152fd66894115848a8f238a817083700bc
SERGIO-PC <vargasleguizamo95@gmail.com>
2024-12-07 16:44:35 -0500

    lol

--- src/index.php
+++ src/index.php
@@ -228,7 +228,7 @@
 <!-- Scripts -->
 <!-- <script src="assets/libs/jquery/dist/jquery.min.js"></script> -->
 <script src="assets/libs/bootstrap/dist/js/bootstrap.js"></script>
-<!-- <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
+<script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- <script src="assets/js/sidebarmenu.js"></script> -->
 <!-- <script src="assets/js/app.min.js"></script> -->
 <!-- <script src="assets/libs/apexcharts/dist/apexcharts.min.js"></script>
